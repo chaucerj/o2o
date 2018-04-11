6469312348aa349bf93460b7cab2595d9b19b8ab
chaucer <767092677@qq.com>
2018-04-11 22:02:09 +0800

    mine

--- src/test/java/dao/AreaDaoTest.java
+++ src/test/java/dao/AreaDaoTest.java
@@ -2,9 +2,11 @@ package dao;
 
 import static org.junit.Assert.assertEquals;
 
-
+import java.text.ParseException;
+import java.util.Date;
 import java.util.List;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -12,50 +14,48 @@ import com.chaucer.o2o.BaseTest;
 import com.chaucer.o2o.dao.AreaDao;
 import com.chaucer.o2o.entity.Area;
 
-public class AreaDaoTest extends BaseTest{
-    @Autowired
-    private  AreaDao areaDao;
-    @Test
-    public void queryArea() {
-        List<Area> areaList = areaDao.queryArea();
-        assertEquals(2,areaList.size());
-    }
+public class AreaDaoTest extends BaseTest {
+	@Autowired
+	private AreaDao areaDao;
 
- 
-//
-//    @Test
-//    @Ignore
-//    public void insertArea() {
-//        Area area = new Area();
-//        area.setAreaName("南苑");
-//        area.setPriority(1);
-//        int effectNumber = areaDao.insertArea(area);
-//        assertEquals(1,effectNumber);
-//    }
-//
-//    @Test
-//    @Ignore
-//    public void updateArea() throws ParseException {
-//        Area area = new Area();
-//        area.setAreaName("西苑");
-//        area.setAreaId(3);
-//        Date date = new Date();
-////        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-////        String d = sdf.format(date);
-////        date = sdf.parse(d);
-//
-//        area.setUpdateTime(date);
-//        int effectNumber = areaDao.updateArea(area);
-//        assertEquals(1,effectNumber);
-//    }
-//
-//    @Test
-//@Ignore
-//    public void deleteArea() {
-//        int effectNumber = areaDao.deleteArea(3);
-//        assertEquals(1,effectNumber);
-//
-//    }
+	@Test
+	public void queryArea() {
+		List<Area> areaList = areaDao.queryArea();
+		assertEquals(2, areaList.size());
+	}
 
+	@Test
+	@Ignore
+	public void insertArea() {
+		Area area = new Area();
+		area.setAreaName("南苑");
+		area.setPriority(1);
+		int effectNumber = areaDao.insertArea(area);
+		assertEquals(1, effectNumber);
+	}
+
+	@Test
+	@Ignore
+	public void updateArea() throws ParseException {
+		Area area = new Area();
+		area.setAreaName("西苑");
+		area.setAreaId(3L);
+		Date date = new Date();
+		// SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		// String d = sdf.format(date);
+		// date = sdf.parse(d);
+
+		area.setUpdateTime(date);
+		int effectNumber = areaDao.updateArea(area);
+		assertEquals(1, effectNumber);
+	}
+
+	@Test
+	@Ignore
+	public void deleteArea() {
+		int effectNumber = areaDao.deleteArea(3);
+		assertEquals(1, effectNumber);
+
+	}
 
 }
